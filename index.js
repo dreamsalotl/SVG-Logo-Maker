fd7467e31d8eb6f115a78bf838fee690408b2340
Donovan Walker <130865355+dreamsalotl@users.noreply.github.com>
2023-08-06 16:59:20 -0500

    mvp

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-// jest questions to take three letters, what shape, and what color
-
+const fs = require('fs');
 const inquirer = require('inquirer');
 const shapes = require('./lib/shapes');
-
 const questions = [
     {
         type: 'input',
@@ -13,16 +11,65 @@ const questions = [
         type: 'list',
         name: 'shape',
         message: 'What shape would you like?',
-        choices: ['square', 'circle', 'triangle']
+        choices: ['Square', 'Circle', 'Triangle', 'Pentagon', 'Hexagon', 'Octagon', 'Star', 'Diamond']
     },
     {
         type: 'input',
         name: 'color',
-        message: 'What color would you like? (keyword or hex code): '
+        message: 'What color would you like the shape to be? (keyword or hex code): '
+    },
+    {
+        type: 'input',
+        name: 'textColor',
+        message: 'What color would you like the text to be? (keyword or hex code): '
     }
 ];
 
-inquirer.prompt(questions).then(answers => {
-    const shape = new shapes[answers.shape](answers.letters, answers.color);
-    shape.draw();
-});
+function writeSVG(answers) {
+    let shape;
+    switch (answers.shape) {
+        case 'Square':
+            shape = new shapes.Square(answers.letters, answers.color);
+            break;
+        case 'Circle':
+            shape = new shapes.Circle(answers.letters, answers.color);
+            break;
+        case 'Triangle':
+            shape = new shapes.Triangle(answers.letters, answers.color);
+            break;
+        case 'Pentagon':
+            shape = new shapes.Pentagon(answers.letters, answers.color);
+            break;
+        case 'Hexagon':
+            shape = new shapes.Hexagon(answers.letters, answers.color);
+            break;
+        case 'Octagon':
+            shape = new shapes.Octagon(answers.letters, answers.color);
+            break;
+        case 'Star':
+            shape = new shapes.Star(answers.letters, answers.color);
+            break;
+        case 'Diamond':
+            shape = new shapes.Diamond(answers.letters, answers.color);
+            break;
+    }
+    let svg = `<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+    <svg width="200" height="200" xmlns="http://www.w3.org/2000/svg">
+        ${shape.draw()}
+        <text x="50%" y="50%" font-size="30px" dominant-baseline="middle" text-anchor="middle" fill="${answers.textColor}">${answers.letters}</text>
+    </svg>`;
+    fs.writeFile('logo.svg', svg, (err) => {
+        if (err) throw err;
+        console.log('The file has been saved!');
+    });
+}
+
+
+function init() {
+    inquirer.prompt(questions).then(answers => {
+        writeSVG(answers);
+    });
+}
+
+
+init();
